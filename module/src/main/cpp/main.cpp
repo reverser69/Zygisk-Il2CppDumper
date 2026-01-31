4a374293b98fd85cd8753149e8012183ea34550c
reverser69 <xmehrx1911@gmail.com>
2026-01-31 05:34:09 +0330

    Update main.cpp
    try to read package name from file

--- module/src/main/cpp/main.cpp
+++ module/src/main/cpp/main.cpp
@@ -15,11 +15,62 @@ using zygisk::Api;
 using zygisk::AppSpecializeArgs;
 using zygisk::ServerSpecializeArgs;
 
+
+// Define the variable with your default
+const char *GamePackageName = "com.your.default.package";
+
+void tryReadPackageNameFromFile() {
+        // Try storage root first (most user-friendly)
+        const char* possiblePaths[] = {
+            "/sdcard/target_package.txt",
+            "/storage/emulated/0/target_package.txt",
+            "/data/adb/modules/zygisk_il2cppdumper/target_package.txt"
+        };
+        
+        for (const char* path : possiblePaths) {
+            if (access(path, R_OK) == 0) {
+                std::ifstream file(path);
+                if (file.is_open()) {
+                    std::string line;
+                    if (std::getline(file, line)) {
+                        // Remove comments (lines starting with #)
+                        size_t commentPos = line.find('#');
+                        if (commentPos != std::string::npos) {
+                            line = line.substr(0, commentPos);
+                        }
+                        
+                        // Trim whitespace
+                        line.erase(0, line.find_first_not_of(" \t\r\n"));
+                        line.erase(line.find_last_not_of(" \t\r\n") + 1);
+                        
+                        if (!line.empty() && line.find_first_not_of('.') != std::string::npos) {
+                            static std::string loadedName = line;
+                            GamePackageName = loadedName.c_str();
+                            LOGI("Il2CppDumper: Loaded package name from %s: %s", 
+                                 path, GamePackageName);
+                            file.close();
+                            return;
+                        }
+                    }
+                    file.close();
+                }
+            }
+        }
+        
+        LOGI("Il2CppDumper: Using default package name: %s", GamePackageName);
+    }
+}
+
 class MyModule : public zygisk::ModuleBase {
 public:
     void onLoad(Api *api, JNIEnv *env) override {
         this->api = api;
         this->env = env;
+
+        // Read package name from file
+        tryReadPackageNameFromFile();
+        
+        LOGI("Il2CppDumper: Module loaded for package: %s", GamePackageName);
     }
 
     void preAppSpecialize(AppSpecializeArgs *args) override {
